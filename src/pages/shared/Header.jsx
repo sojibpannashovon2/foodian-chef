3cbeeb5de8d1f910d52008d08eec7775ce91dbf2
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-05-04 14:26:43 +0600

    logout complete with user pictur

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,15 +1,30 @@
 // import React from 'react';
 
-import { Card, Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Card, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaPersonBooth } from "react-icons/fa";
+// import { FaPersonBooth } from "react-icons/fa";
 
 import banner from '../../assets/banner.jpg'
+import { useContext } from "react";
+import { authContext } from "../../provider/AuthProvider";
 
 const Header = () => {
+
+    const { user, logOut } = useContext(authContext)
+    // console.log(name);
+
+    const handleLogoutButton = () => {
+        logOut()
+            .then(() => {
+                alert("Sign-Out Successfully")
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <>
-            <Navbar className="container bg-secondary p-2 border border-success shadow-lg fw-bold" collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar className="container bg-secondary p-2 border border-success shadow-lg fw-bold " collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
                     <Navbar.Brand className="fw-bold" href="#home">Foodian Chef-And-Food</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -24,11 +39,30 @@ const Header = () => {
                             <Nav.Link href="#pricin2">About</Nav.Link>
 
                         </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets"><FaPersonBooth></FaPersonBooth></Nav.Link>
-                            <Nav.Link href="#memes">
-                                <Link to='/login'>Log-In</Link>
+                        <Nav className='d-flex justify-content-center align-items-center fw-bold'>
+                            {/* {
+                                user && <Nav.Link href="#deets"><FaPersonBooth style={{ fontSize: "2rem" }}></FaPersonBooth></Nav.Link>
+                            } */}
+
+                            <div className="">
+                                {
+                                    user && <p> <img className="rounded-circle" style={{ height: "40px", width: "40px" }} src={user?.photoURL} alt="Not Found" /></p>
+                                }
+                            </div>
+
+                            <Nav.Link eventKey={2} href="#memes">
+                                {
+                                    user ? <><span className="me-2">{user.email}</span><Button variant="success" onClick={handleLogoutButton}>Log-Out</Button></> :
+
+                                        <Link to="/login">
+
+                                            <Button variant="secondary">Log-In</Button>
+                                        </Link>
+                                }
+
+
                             </Nav.Link>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/pages/shared/NavberPd.jsx
+++ src/pages/shared/NavberPd.jsx
@@ -1,10 +1,25 @@
 // import React from 'react';
 
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaPersonBooth } from "react-icons/fa";
+import { useContext } from "react";
+import { authContext } from "../../provider/AuthProvider";
 
 const NavberPd = () => {
+
+    const { user, logOut } = useContext(authContext)
+    // console.log(name);
+
+    const handleLogoutButton = () => {
+        logOut()
+            .then(() => {
+                alert("Sign-Out Successfully")
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <Navbar className="container bg-secondary p-2 border border-success shadow-lg fw-bold" collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
@@ -21,12 +36,28 @@ const NavberPd = () => {
                         <Nav.Link href="#pricin2">About</Nav.Link>
 
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets"><FaPersonBooth></FaPersonBooth></Nav.Link>
-                        <Nav.Link href="#memes">
+                    <Nav className='d-flex justify-content-center align-items-center fw-bold gap-2'>
+                        {/* {
+                            user && <Nav.Link href="#deets"><FaPersonBooth style={{ fontSize: "2rem" }}></FaPersonBooth></Nav.Link>
+                        } */}
+                        <div className="">
+                            {
+                                user && <p> <img className="rounded-circle" style={{ height: "40px", width: "40px" }} src={user?.photoURL} alt="Not Found" /></p>
+                            }
+                        </div>
+                        <Nav.Link eventKey={2} href="#memes">
+                            {
+                                user ? <><span className="me-2">{user.email}</span><Button variant="success" onClick={handleLogoutButton}>Log-Out</Button></> :
 
-                            <Link to="/login">Log-In</Link>
+                                    <Link to="/login">
+
+                                        <Button variant="secondary">Log-In</Button>
+                                    </Link>
+                            }
+
+
                         </Nav.Link>
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
